8677300d328de53f75387ddfb8bd9dade3cf3199
E15dev <85256998+E15dev@users.noreply.github.com>
2022-06-30 01:43:58 +0200

    Update C4Music18.js

--- C4Music18.js
+++ C4Music18.js
@@ -1,70 +1,19 @@
 /**
  * @name C4Music18
  * @version 0.1.0
+ * @author XerwinXpl
  * @authorLink https://twitter.com/XerwinXpl
- * @website 
- * @source 
+ * @website https://github.com/E15dev/C4Music18
+ * @source https://raw.githubusercontent.com/E15dev/C4Music18/main/C4Music18.js
  */
 
-class Example {
-    // Constructor
-    constructor() {
-        this.initialized = false;
-    }
-
-    // Meta
-    getName() { return "C418 Music"; }
-    getShortName() { return "C4Music18"; }
-    getDescription() { return "play random C418 tracks."; }
-    getVersion() { return "0.1.0"; }
-    getAuthor() { return "XerwinXpl"; }
-
-    // Settings  Panel
-    getSettingsPanel() {
-        return "<!--Enter Settings Panel Options, just standard HTML-->";
-    }
-    
-    // Load/Unload
-    load() { }
-
-    unload() { }
-
-    // Events
-
-    onMessage() {
-        // Called when a message is received
-    };
-
-    onSwitch() {
-        // Called when a server or channel is switched
-    };
-
-    observer(e) {
-        // raw MutationObserver event for each mutation
-    };
-    
-    // Start/Stop
+module.exports = class C4Music18 {
+    load() {}
     start() {
-        var libraryScript = document.getElementById('zeresLibraryScript');
-	if (!libraryScript) {
-		libraryScript = document.createElement("script");
-		libraryScript.setAttribute("type", "text/javascript");
-		libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
-		libraryScript.setAttribute("id", "zeresLibraryScript");
-		document.head.appendChild(libraryScript);
-	}
-
-	if (typeof window.ZeresLibrary !== "undefined") this.initialize();
-	else libraryScript.addEventListener("load", () => { this.initialize(); });
+        BdApi.showToast("test!")
     }
-       
-    stop() {
-        PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has stopped.");
-    };
 
-    //  Initialize
-    initialize() {
-        this.initialized = true;
-        PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has started.");
+    stop() {
+
     }
 }
