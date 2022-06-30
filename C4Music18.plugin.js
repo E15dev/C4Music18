03ad2d1b09059f53a840698dbee3cb746040fb92
E15dev <85256998+E15dev@users.noreply.github.com>
2022-06-30 13:07:40 +0200

    Update C4Music18.plugin.js

--- C4Music18.plugin.js
+++ C4Music18.plugin.js
@@ -8,14 +8,22 @@
  * @source https://raw.githubusercontent.com/E15dev/C4Music18/main/C4Music18.plugin.js
  */
 
+const test_audio_link = 'https://t4.bcbits.com/stream/433394b5867317aecb383601f830efef/mp3-128/1603173650?p=0&amp;ts=1656633589&amp;t=63e32ea177cedd9754ead49d0b1e98e7e459434d&amp;token=1656633589_01f13e3c15873007c7389c61c1fae653c2c28fe1&quot;'
+
 module.exports = class C4Music18 {
 
     load() {}
     start() {
         BdApi.showToast("starting!");
+        play(test_audio_link)
     }
 
     stop() {
 
     }
+}
+
+function play(a_link) {
+  var audio = new Audio(a_link);
+  audio.play();
 }
