3731694e9ef6ffdddbdaa3c1f039d6f16e304469
E15dev <85256998+E15dev@users.noreply.github.com>
2022-06-30 02:04:22 +0200

    bc stream test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 function play() {
-  var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+  var audio = new Audio('https://t4.bcbits.com/stream/433394b5867317aecb383601f830efef/mp3-128/1603173650?p=0&amp;ts=1656633589&amp;t=63e32ea177cedd9754ead49d0b1e98e7e459434d&amp;token=1656633589_01f13e3c15873007c7389c61c1fae653c2c28fe1&quot;');
   audio.play();
 }
