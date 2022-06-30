0f2e0dc38bb10025cbdc73879986d36fb9e91731
E15dev <85256998+E15dev@users.noreply.github.com>
2022-06-30 13:03:26 +0200

    Create C4Music18.plugin.js

---
+++ C4Music18.plugin.js
@@ -0,0 +1,21 @@
+/**
+ * @name C4Music18
+ * @version 0.1.0
+ * @author XerwinXpl
+ * @description play C418 music
+ * @authorLink https://twitter.com/XerwinXpl
+ * @website https://github.com/E15dev/C4Music18
+ * @source https://raw.githubusercontent.com/E15dev/C4Music18/main/C4Music18.plugin.js
+ */
+
+module.exports = class C4Music18 {
+
+    load() {}
+    start() {
+        BdApi.showToast("starting!");
+    }
+
+    stop() {
+
+    }
+}
